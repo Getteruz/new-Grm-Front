ef182c56601a7a964bcdee0d63bf95cab0fbf33e
abdiganii <abduganiabdu2@gmail.com>
2025-04-14 19:56:59 +0500

    add PWA to admin oabel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,38 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest:{
+        name: "Seller App",
+        short_name: "Seller App",
+        icons: [
+            {
+                src: "/favicon/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png"
+            },
+            {
+                src: "/favicon/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png"
+            }
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        start_url: "/",
+        display: "standalone"
+    },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

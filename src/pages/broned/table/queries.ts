9eaeda2fd4e35e738fe26da2c4604be6a6cf119a
Nodirbek Sodiqov <nodirbeksodiqov122@gmail.com>
2025-04-19 20:20:08 +0500

    asd
    Signed-off-by: Nodirbek Sodiqov <nodirbeksodiqov122@gmail.com>

--- src/pages/broned/table/queries.ts
+++ src/pages/broned/table/queries.ts
@@ -1,20 +1,13 @@
-import { DefinedInitialDataOptions, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 import { getAllData } from "@/service/apiHelpers";
 import { apiRoutes } from "@/service/apiRoutes";
-import { TResponse } from "@/types";
-import { BronedData, BronedQuery } from "../type";
 
-interface IBroned {
-  options?: DefinedInitialDataOptions<TResponse<BronedData>>;
-  queries?: BronedQuery;
-}
-const useBroned = ({ options, queries }: IBroned) =>
+const useBroned = ({ options, queries }) =>
   useQuery({
     ...options,
     queryKey: [apiRoutes.broned, queries],
-    queryFn: () =>
-      getAllData<TResponse<BronedData>, BronedQuery>(apiRoutes.broned, queries),
+    queryFn: () => getAllData(apiRoutes.broned, queries),
   });
 
 export default useBroned;

c95410e3023433ea85f159075665ce22cf85ff0d
Nodirbek Sodiqov <nodirbeksodiqov122@gmail.com>
2025-04-30 12:45:20 +0500

    asd
    Signed-off-by: Nodirbek Sodiqov <nodirbeksodiqov122@gmail.com>

--- src/pages/statement/table/StatusBadge.tsx
+++ src/pages/statement/table/StatusBadge.tsx
@@ -2,7 +2,7 @@ import { Badge } from "@/components/ui/badge";
 
 export const StatusBadge = ({ status }: { status: string }) => {
   let variant: "default" | "secondary" | "destructive" | "outline" = "default";
-  
+
   switch (status) {
     case "Отправить":
       variant = "default"; // Green
@@ -19,9 +19,12 @@ export const StatusBadge = ({ status }: { status: string }) => {
     default:
       variant = "default";
   }
-  
+
   return (
-    <Badge variant={variant} className={`w-[93px] py-[12px] rounded-full px-[16px] gap-[10px] justify-center ${status === "Отправить" ?"bg-[#89A143] text-white": status === "Отказана" ? "bg-[#E38157] text-white border-[#E38157]" : status === "В процессе" ? "border-[#E38157] text-[#E38157]" : "border-[#CBCBC1] text-[#CBCBC1]"}`}>
+    <Badge
+      variant={variant}
+      className={`min-w-[93px] py-[12px] rounded-full px-[16px] gap-[10px] justify-center ${status === "Отправить" ? "bg-[#89A143] text-white" : status === "Отказана" ? "bg-[#E38157] text-white border-[#E38157]" : status === "В процессе" ? "border-[#E38157] text-[#E38157]" : "border-[#CBCBC1] text-[#CBCBC1]"}`}
+    >
       {status}
     </Badge>
   );

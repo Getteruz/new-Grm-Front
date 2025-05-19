32dd9958ca6d50b41174bcb03f818b56fbf82cbd
khamidullayevdev <khamidullayevammoriddin@gmail.com>
2025-05-19 12:11:24 +0500

    Statement delete function

--- src/pages/statement/table/index.tsx
+++ src/pages/statement/table/index.tsx
@@ -42,7 +42,6 @@ export default function Page() {
         fetchNextPage={fetchNextPage}
         hasNextPage={hasNextPage ?? false}
         isFetchingNextPage={isFetchingNextPage}
-        isRowClickble
       />
 
       <CreateStatementModal isOpen={id === "new"} onClose={() => setId(null)} />

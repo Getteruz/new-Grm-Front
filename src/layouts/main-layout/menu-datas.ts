a158a1ae92ff13973436c055d8c817367b4d7f08
abdiganii <abduganiabdu2@gmail.com>
2025-08-07 13:08:31 +0500

    fixed

--- src/layouts/main-layout/menu-datas.ts
+++ src/layouts/main-layout/menu-datas.ts
@@ -612,6 +612,11 @@ export const DataMenu = {
           link: "/m-manager/report-seller",
           text: "Отчёт по сотрудикам",
         },
+        {
+          id: 26,
+          link: "/m-manager/report-orginal",
+          text: "Хисобот",
+        },
       ],
     },
     {

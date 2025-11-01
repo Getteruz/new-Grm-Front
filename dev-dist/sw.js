6ff470126e3401beabe32ec8952e2569213a8d7d
abdiganii <abduganiabdu2@gmail.com>
2025-11-01 12:31:18 +0500

    fixed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vk5p30c6m7o"
+    "revision": "0.roo9ps50ol"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
